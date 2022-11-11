267ddb75c4df2b2a58a02cc0cc7954d213c6c357
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-11-11 14:34:22 +0200

    added ability to render weather icons properly

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ var humidity = document.getElementById("humidity");
 var windSpeed = document.getElementById("wind-speed");
 var uvIndex = document.getElementById("UV-index")
 var cityName = document.getElementById("city-name");
+var history = document.getElementById("history");
+var searchHistory = JSON.parse(localStorage.getItem("history"));
+console.log(searchHistory);
 
 function getWeather(searchCity) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + "84b79da5e5d7c92085660485702f4ce8";
@@ -17,14 +20,12 @@ function getWeather(searchCity) {
         .then(function (data) {
             console.log(data);
             for(var i = 0; i < data.lenght; i++);
-            let weatherPic = data.weather[0].icon;
-            icon.setAttribute("src", "https://openweathermap.org/img/w/" + weatherPic);
+            icon.setAttribute("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
             currentTemp.textContent = "Temperature: " + data.main.temp + " °F";
             humidity.textContent = "Humidity: " + data.main.humidity + " %";
             windSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH";
             var currentDate = (moment().format("dddd, MMMM Do"));
-            cityName.textContent = data.name + currentDate;
-            
+            cityName.textContent = data.name + currentDate;  
     });
 };
 
