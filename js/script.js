2283f16643153bfd6f54351033e57d1718821ec0
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-11-11 13:24:53 +0200

    added possibility to display current weather

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 var searchCity = document.getElementById("search-city");
 var searchButton = document.getElementById("search-button");
+var currentTemp = document.getElementById("temperature");
+var icon = document.getElementById("icon");
+var humidity = document.getElementById("humidity");
+var windSpeed = document.getElementById("wind-speed");
+var uvIndex = document.getElementById("UV-index")
+var cityName = document.getElementById("city-name");
 
-var getWeather = function (searchCity) {
+function getWeather(searchCity) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + "84b79da5e5d7c92085660485702f4ce8";
     fetch(apiUrl)
         .then(function (response) {
@@ -10,7 +16,16 @@ var getWeather = function (searchCity) {
         })
         .then(function (data) {
             console.log(data);
-        });
+            for(var i = 0; i < data.lenght; i++);
+            let weatherPic = data.weather[0].icon;
+            icon.setAttribute("src", "https://openweathermap.org/img/w/" + weatherPic);
+            currentTemp.textContent = "Temperature: " + data.main.temp + " °F";
+            humidity.textContent = "Humidity: " + data.main.humidity + " %";
+            windSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH";
+            var currentDate = (moment().format("dddd, MMMM Do"));
+            cityName.textContent = data.name + currentDate;
+            
+    });
 };
 
 var formSubmitHandler = function (event) {
